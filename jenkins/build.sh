8418f7ca6d437ea8d78947b4fe54d7b052fba2a6
Byron McCollum <byron.mccollum@rackspace.com>
2015-09-16 16:46:40 -0500

    Parameterize RPC Heat Ansible Release.

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -8,14 +8,10 @@ sudo pip install oslo.config
 # Environment Binding
 $PUBLIC_CLOUD_CREDENTIALS
 
-HEAT_TEMPLATE=${HEAT_TEMPLATE:-"compute"}
-ANSIBLE_TAGS=${ANSIBLE_TAGS:-"prepare"}
-RPC_RELEASE=${RPC_RELEASE:-"r11.0.0"}
 HEAT_TEMPLATE_VERSION=`echo $RPC_RELEASE | sed 's/^r//g' | awk -F '[\.]' '{ print $1 "." $2 }'`
-
 STACK_NAME=rpc-jenkins-$BUILD_NUMBER-$HEAT_TEMPLATE-install-`echo $RPC_RELEASE | sed 's/\./-/g'`-$ANSIBLE_TAGS
 
-heat stack-create -f rpc-$HEAT_TEMPLATE_VERSION-$HEAT_TEMPLATE-template.yml -t 240 -P rpc_release=$RPC_RELEASE -P ansible_tags=`echo $ANSIBLE_TAGS | sed 's/-/,/g'` -e $HEAT_ENVIRONMENT $STACK_NAME
+heat stack-create -f rpc-$HEAT_TEMPLATE_VERSION-$HEAT_TEMPLATE-template.yml -t 240 -P rpc_release=$RPC_RELEASE -P ansible_tags=`echo $ANSIBLE_TAGS | sed 's/-/,/g'` -P rpc_heat_ansible_release=$RPC_HEAT_ANSIBLE_RELEASE -e $HEAT_ENVIRONMENT $STACK_NAME
 
 BUILD_COMPLETED=0
 BUILD_FAILED=0
