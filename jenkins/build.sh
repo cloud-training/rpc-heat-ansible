dd3de14fe8fbbc134907132d98b509a3c0dbec1b
Byron McCollum <byron.mccollum@rackspace.com>
2015-10-30 10:23:40 +0900

    Retry stack delete if status is not DELETE_IN_PROGRESS.

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -69,7 +69,7 @@ until [[ $BUILD_DELETED -eq 0 ]]; do
   echo "===================================================="
   echo "Stack Status:        $STACK_STATUS"
   echo "Build Deleted:       $BUILD_DELETED"
-  if [[ "$STACK_STATUS" == 'DELETE_FAILED' ]]; then
+  if [[ "$STACK_STATUS" != 'DELETE_IN_PROGRESS' ]]; then
     heat stack-delete $STACK_NAME
   fi
 done
